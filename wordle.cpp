2dc349b4f2a9c1bbbb58db99b01829a5f5c70095
Gamma_02 <gposselt@usc.edu>
2025-04-17 15:51:21 -0700

    finished wordle

--- wordle.cpp
+++ wordle.cpp
@@ -8,11 +8,24 @@
 #endif
 
 #include "wordle.h"
+
+#include <vector>
+
 #include "dict-eng.h"
 using namespace std;
 
 
 // Add prototypes of helper functions here
+
+std::string strtolower(const std::string& str) {
+    std::string output{};
+
+    for (char c : str) {
+        output += static_cast<char>(tolower(c));
+    }
+
+    return output;
+}
 
 
 // Definition of primary wordle function
@@ -23,6 +36,73 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+    std::vector<word_filter*> filters{};
+
+    filters.push_back(new word_size_filter{in.size()});
+
+
+    std::vector<static_location_filter*> location_filters{};
+    for (size_t i = 0; i < in.size(); i++) {
+        const char c = in[i];
+        if (c == '-') continue; //skip empty characters
+
+        location_filters.push_back(new static_location_filter{i, c});
+    }
+
+    std::map<char, int> floating_letter_occurances{};
+
+    for (const char& a : floating) {
+        char c = a;
+
+
+        if (floating_letter_occurances.find(c) == floating_letter_occurances.end()) {
+            floating_letter_occurances.insert(std::make_pair(c, 0));
+        }
+
+        floating_letter_occurances[c] = floating_letter_occurances[c] + 1;
+
+    }
+
+    for (pair<const char, int> letter : floating_letter_occurances) {
+        floating_letter_filter* filter = new floating_letter_filter{letter.first, letter.second, location_filters};
+
+        filters.push_back(filter);
+    }
+
+    std::vector<word_filter*>::iterator before_floating = filters.begin();
+    ++before_floating;
+    filters.insert(before_floating, location_filters.begin(), location_filters.end());
+
+    std::set<std::string> toRemove{};
+    for (const std::string& word : dict) {
+        for (const word_filter* filter : filters) {
+            if (!(*filter)(word)) {
+                toRemove.insert(word);
+                break;
+            }
+        }
+    }
+
+
+    std::set<std::string> result{};
+
+
+    for (const auto & it : dict) {
+
+        if (toRemove.find(it) == toRemove.end()) {
+
+            std::string a = strtolower(it);
+
+            if (a != it) {
+                continue;
+            }
+
+            result.insert(strtolower(it));
+        }
+
+    }
+
+    return result;
 }
 
 // Define any helper functions here
--- wordle.h
+++ wordle.h
@@ -5,6 +5,7 @@
 #include <set>
 #include <string>
 #endif
+#include <vector>
 
 /**
  * @brief Returns the list of all legal words in the provided dictionary
@@ -21,5 +22,99 @@ std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict);
+
+struct word_filter {
+    virtual ~word_filter() = default;
+
+    virtual bool operator()(const std::string& word) const = 0;
+};
+
+struct location_filter : word_filter {
+    /**
+     * Override to make it easier to code :3
+     * @param word The word test to match
+     * @return false iff the word could not possibly be the wordle word according to this filter
+     */
+    bool operator()(const std::string& word) const override {
+        for (size_t i = 0; i < word.size(); i++) {
+            char letter = word[i];
+
+            if (!(*this)(i, letter)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     *
+     * @param locationInWord The location in a test word that this filter is getting applied at
+     * @param letterAtLocation The letter at that location in the test word
+     * @return false iff the word could not possibly be the wordle word according to this filter
+     */
+    virtual bool operator()(size_t locationInWord, char letterAtLocation) const = 0;
+};
+
+struct static_location_filter : location_filter {
+    size_t location;
+    char letter;
+
+    static_location_filter(size_t location, char letter) : location(location), letter(letter) {}
+
+    bool operator()(size_t locationInWord, char letterAtLocation) const override {
+        if (locationInWord != this->location) {
+            return true;
+        }
+
+        return letterAtLocation == letter;
+    }
+};
+
+struct floating_letter_filter : word_filter {
+    char letter;
+    int occurrences;
+    const std::vector<static_location_filter*>& filters;
+
+    floating_letter_filter(char letter, int occurrences, const std::vector<static_location_filter*>& locations_to_avoid) : letter(letter), occurrences(occurrences), filters(locations_to_avoid) {}
+
+    bool operator()(const std::string& word) const override {
+        int letter_count = 0;
+
+        for (size_t i = 0; i < word.size(); i++) {
+            const char c = word[i];
+
+            bool cont = false;
+
+            for (const static_location_filter* locFilter : filters) {
+                if (locFilter->location == i && (*locFilter)(i, c)) {
+                    cont = true;
+                    break;
+                }
+            }
+
+            if (cont) {
+                continue;
+            }
+
+            if (c == letter) {
+                letter_count++;
+            }
+        }
+
+        return letter_count >= occurrences;
+    }
+
+};
+
+struct word_size_filter : word_filter {
+    size_t size;
+
+    explicit word_size_filter(size_t wordSize) : size(wordSize) {}
+
+    bool operator()(const std::string& word) const override {
+        return word.size() == size;
+    }
+
+};
 
 #endif
